9f198ff228df2b2de6c1d4e7f7a6a5d0bf596f84
André Costa <andrefonsecacosta@gmail.com>
2023-07-21 16:39:32 -0700

    Fix `Schema` type

--- src/schema.ts
+++ src/schema.ts
@@ -1,15 +1,12 @@
 import type {InferSchema} from './resolver';
 import type {IfDefined} from './utils';
 
-export type Schema =
-  | {
-      [K in keyof TypeSchemaRegistry]: IfDefined<
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        InferSchema<TypeSchemaRegistry[K], any>
-      >;
-    }[keyof TypeSchemaRegistry]
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  | TypeSchema<any>;
+export type Schema = {
+  [K in keyof TypeSchemaRegistry]: IfDefined<
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    InferSchema<TypeSchemaRegistry[K], any>
+  >;
+}[keyof TypeSchemaRegistry];
 
 export const Source = Symbol.for('TypeSchema.Source');
 
