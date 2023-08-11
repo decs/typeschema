8984334dc516ce717af33714763a64932795934b
André Costa <andrefonsecacosta@gmail.com>
2023-08-11 16:28:56 -0700

    Expose vite submodule

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,18 @@
 import {defineConfig} from 'tsup';
 
-export default defineConfig({
-  clean: true,
-  dts: true,
-  entry: ['src/index.ts', 'src/vite/index.ts', 'src/vite/placeholder.ts'],
-  format: ['esm', 'cjs'],
-  tsconfig: 'tsconfig.build.json',
-});
+export default defineConfig([
+  {
+    clean: true,
+    dts: true,
+    entry: ['src/index.ts'],
+    format: ['esm', 'cjs'],
+    onSuccess: "sed -i '' 's/import/require/g' dist/index.js",
+  },
+  {
+    clean: true,
+    dts: true,
+    entry: ['src/vite/index.ts', 'src/vite/placeholder.ts'],
+    format: 'cjs',
+    outDir: 'vite',
+  },
+]);
