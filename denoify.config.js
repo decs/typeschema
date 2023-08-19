c1d270a5db616218001be419a7a0f18931482a9c
André Costa <andrefonsecacosta@gmail.com>
2023-08-18 18:12:55 -0700

    Add deno support

---
+++ denoify.config.js
@@ -0,0 +1,9 @@
+/** @type { import('denoify/lib/config/parseParams').DenoifyParams } */
+const config = {
+  ports: {
+    valibot: 'https://deno.land/x/valibot/mod.ts',
+    zod: 'https://deno.land/x/zod/mod.ts',
+  },
+};
+
+module.exports = config;
