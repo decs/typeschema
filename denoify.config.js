3a515e78ee5a589d7853b5567c5dfc16bb701484
André Costa <andrefonsecacosta@gmail.com>
2023-08-31 21:09:34 -0700

    Fix readme on Deno

--- denoify.config.js
+++ denoify.config.js
@@ -1,6 +1,6 @@
 /** @type { import('denoify/lib/config/parseParams').DenoifyParams } */
 const config = {
-  includes: ['assets'],
+  includes: ['LICENSE', 'README.md', 'assets'],
   ports: {
     valibot: 'https://deno.land/x/valibot/mod.ts',
     zod: 'https://deno.land/x/zod/mod.ts',
