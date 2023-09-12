fdb461cacdd65f15ed89bdbd5ca551c644f2b351
actions <actions@github.com>
2023-09-12 20:02:31 +0000

    Add deno distribution files

---
+++ deno_dist/__tests__/joi.test.ts
@@ -0,0 +1,69 @@
+import type {Infer, InferIn} from '../index.ts';
+
+import {describe, expect, test} from 'node:@jest/globals';
+import {initTRPC} from 'npm:@trpc/server@10.38.1';
+import {expectTypeOf} from 'npm:expect-type@0.16.0';
+import Joi from 'npm:joi@17.10.1';
+
+import {assert, validate, wrap} from '../index.ts';
+
+describe('joi', () => {
+  const schema = Joi.object({
+    age: Joi.number().required(),
+    createdAt: Joi.date().required(),
+    email: Joi.string().email().required(),
+    id: Joi.string().required(),
+    name: Joi.string().required(),
+    updatedAt: Joi.date().required(),
+  });
+
+  const data = {
+    age: 123,
+    createdAt: new Date('2021-01-01T00:00:00.000Z'),
+    email: 'john.doe@test.com',
+    id: '123',
+    name: 'John Doe',
+    updatedAt: new Date('2021-01-01T00:00:00.000Z'),
+  };
+  const badData = {
+    age: 123,
+    createdAt: '2021-01-01T00:00:00.000Z',
+    email: 'john.doe',
+    id: '123',
+    name: 'John Doe',
+    updatedAt: '2021-01-01T00:00:00.000Z',
+  };
+
+  test('infer', () => {
+    expectTypeOf<Infer<typeof schema>>().toEqualTypeOf<unknown>();
+    expectTypeOf<InferIn<typeof schema>>().toEqualTypeOf<unknown>();
+  });
+
+  test('validate', async () => {
+    expect(await validate(schema, data)).toStrictEqual({
+      data,
+      success: true,
+    });
+    expect(await validate(schema, badData)).toStrictEqual({
+      issues: [{message: '"email" must be a valid email', path: ['email']}],
+      success: false,
+    });
+  });
+
+  test('assert', async () => {
+    expect(await assert(schema, data)).toStrictEqual(data);
+    await expect(assert(schema, badData)).rejects.toThrow();
+  });
+
+  test('wrap', async () => {
+    const tRPC = initTRPC.create();
+    const router = tRPC.router({
+      hello: tRPC.procedure.input(wrap(schema)).query(({input}) => {
+        expectTypeOf<typeof input>().toEqualTypeOf<unknown>();
+        return input;
+      }),
+    });
+    const caller = router.createCaller({});
+    expect(await caller.hello(data)).toStrictEqual(data);
+  });
+});
---
+++ deno_dist/__tests__/valibot.test.ts
@@ -0,0 +1,90 @@
+import type {Infer, InferIn} from '../index.ts';
+
+import {beforeEach, describe, expect, jest, test} from 'node:@jest/globals';
+import {initTRPC} from 'npm:@trpc/server@10.38.1';
+import {expectTypeOf} from 'npm:expect-type@0.16.0';
+import {email, number, object, string, transform} from 'https://deno.land/x/valibot@v0.15.0/mod.ts';
+
+import {assert, validate, wrap} from '../index.ts';
+import {fetchModule} from '../adapters/valibot.ts';
+
+describe('valibot', () => {
+  const schema = object({
+    age: number(),
+    createdAt: transform(string(), value => new Date(value)),
+    email: string([email()]),
+    id: string(),
+    name: string(),
+    updatedAt: transform(string(), value => new Date(value)),
+  });
+  const module = '../adapters/modules/valibot';
+
+  const data = {
+    age: 123,
+    createdAt: '2021-01-01T00:00:00.000Z',
+    email: 'john.doe@test.com',
+    id: 'c4a760a8-dbcf-4e14-9f39-645a8e933d74',
+    name: 'John Doe',
+    updatedAt: '2021-01-01T00:00:00.000Z',
+  };
+  const outputData = {
+    age: 123,
+    createdAt: new Date('2021-01-01T00:00:00.000Z'),
+    email: 'john.doe@test.com',
+    id: 'c4a760a8-dbcf-4e14-9f39-645a8e933d74',
+    name: 'John Doe',
+    updatedAt: new Date('2021-01-01T00:00:00.000Z'),
+  };
+  const badData = {
+    age: '123',
+    createdAt: '2021-01-01T00:00:00.000Z',
+    email: 'john.doe@test.com',
+    id: 'c4a760a8-dbcf-4e14-9f39-645a8e933d74',
+    name: 'John Doe',
+    updatedAt: '2021-01-01T00:00:00.000Z',
+  };
+
+  beforeEach(() => fetchModule.clear());
+
+  test('infer', () => {
+    expectTypeOf<Infer<typeof schema>>().toEqualTypeOf(outputData);
+    expectTypeOf<InferIn<typeof schema>>().toEqualTypeOf(data);
+  });
+
+  test('validate', async () => {
+    expect(await validate(schema, data)).toStrictEqual({
+      data: outputData,
+      success: true,
+    });
+    expect(await validate(schema, badData)).toStrictEqual({
+      issues: [{message: 'Invalid type', path: ['age']}],
+      success: false,
+    });
+  });
+
+  test('assert', async () => {
+    expect(await assert(schema, data)).toStrictEqual(outputData);
+    await expect(assert(schema, badData)).rejects.toThrow();
+  });
+
+  test('wrap', async () => {
+    const tRPC = initTRPC.create();
+    const router = tRPC.router({
+      hello: tRPC.procedure.input(wrap(schema)).query(({input}) => {
+        expectTypeOf<typeof input>().toEqualTypeOf(outputData);
+        return input;
+      }),
+    });
+    const caller = router.createCaller({});
+    expect(await caller.hello(data)).toStrictEqual(outputData);
+  });
+
+  test('peer dependency', async () => {
+    jest.mock(module, () => {
+      throw new Error('Cannot find module');
+    });
+    await expect(validate(schema, data)).rejects.toThrow();
+    await expect(assert(schema, data)).rejects.toThrow();
+    jest.unmock(module);
+  });
+});
