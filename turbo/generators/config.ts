ca712b930d26b32961002d666411d2a777d66567
André Costa <andrefonsecacosta@gmail.com>
2024-01-31 13:10:57 -0800

    Make the adapters codegen generic

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,19 +1,31 @@
 import type {PlopTypes} from '@turbo/gen';
 
-export const adapterNames = ['main', 'valibot', 'zod'];
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+
+const nonAdapterPackageNames = ['core', 'typeschema'];
+
+function getPackageNames(plop: PlopTypes.NodePlopAPI): Array<string> {
+  const packagesPath = path.join(plop.getDestBasePath(), 'packages');
+  const packagesDir = fs.readdirSync(packagesPath, {withFileTypes: true});
+  return packagesDir.filter(file => file.isDirectory()).map(file => file.name);
+}
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
+  const packageNames = getPackageNames(plop);
+  const adapterNames = packageNames.filter(
+    packageName => !nonAdapterPackageNames.includes(packageName),
+  );
+
   plop.setGenerator('adapters', {
-    actions: [
-      ...adapterNames.flatMap(adapterName => [
-        {
-          force: true,
-          path: 'packages/' + adapterName + '/src/index.ts',
-          templateFile: 'templates/index.ts.hbs',
-          type: 'add',
-        },
-      ]),
-    ],
+    actions: adapterNames.flatMap(adapterName => [
+      {
+        force: true,
+        path: 'packages/' + adapterName + '/src/index.ts',
+        templateFile: 'templates/index.ts.hbs',
+        type: 'add',
+      },
+    ]),
     description: 'Generates common adapter files',
     prompts: [],
   });
