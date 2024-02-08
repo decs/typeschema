4380d2a06520253a87a0000f06728035874089d9
André Costa <andrefonsecacosta@gmail.com>
2024-02-07 23:47:49 -0800

    Added example for testing webpack and turbopack

---
+++ examples/next/src/app/layout.tsx
@@ -0,0 +1,17 @@
+import type {Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: 'Next',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
