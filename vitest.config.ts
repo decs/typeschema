31cf857817cc40c351131edbce7494c0aa188a5b
André Costa <andrefonsecacosta@gmail.com>
2024-02-18 00:10:37 -0800

    Fix tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,18 @@
-import {defineConfig} from 'vitest/config';
+import {deepkitType} from '@deepkit/vite';
 import typescript from 'rollup-plugin-typescript2';
+import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
   esbuild: false,
-  plugins: [typescript({tsconfig: './tsconfig.test.json'})],
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.test.json',
+      exclude: './**/deepkit.test.ts',
+    }),
+    deepkitType({
+      tsConfig: './tsconfig.test.json',
+      include: './**/deepkit.test.ts',
+    }),
+  ],
   test: {watch: false},
 });
