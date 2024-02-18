0c4e758a8646fed6d97fc54b60f3756a24a014ab
André Costa <andrefonsecacosta@gmail.com>
2024-02-18 14:19:00 -0800

    Move next package to /examples

--- bundlers/turbopack/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import {validate} from '@typeschema/main';
-import {z} from 'zod';
-
-export default async function Page() {
-  const result = await validate(z.string(), 'hello');
-  return result.success
-    ? result.data
-    : result.issues.map(({message}) => message).join('\n');
-}
--- examples/next/src/app/layout.tsx
+++ examples/next/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type {Metadata} from 'next';
 
 export const metadata: Metadata = {
-  title: 'Next',
+  title: 'TypeSchema',
 };
 
 export default function RootLayout({
