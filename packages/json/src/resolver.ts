8484a72d416f7173bc49c280142622283241b25b
André Costa <andrefonsecacosta@gmail.com>
2024-02-26 00:32:27 -0800

    Infer types for json schemas

--- packages/json/src/resolver.ts
+++ packages/json/src/resolver.ts
@@ -1,8 +1,13 @@
 import type {IfDefined, Resolver} from '@typeschema/core';
 import type {SchemaObject} from 'ajv';
+import type {FromSchema} from 'json-schema-to-ts';
 
 export interface AdapterResolver extends Resolver {
-  base: IfDefined<SchemaObject, 'ajv'>;
-  input: this['schema'] extends this['base'] ? unknown : never;
-  output: this['schema'] extends this['base'] ? unknown : never;
+  base: IfDefined<Readonly<SchemaObject>, 'ajv'>;
+  input: this['schema'] extends this['base']
+    ? IfDefined<FromSchema<this['schema']>, 'json-schema-to-ts'>
+    : never;
+  output: this['schema'] extends this['base']
+    ? IfDefined<FromSchema<this['schema']>, 'json-schema-to-ts'>
+    : never;
 }
