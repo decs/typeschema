009db13744001ffbbee5b3883748add9972b8f36
André Costa <andrefonsecacosta@gmail.com>
2024-02-26 22:28:27 -0800

    Revert "Replace missing imported types with an error message"
    This reverts commit 082090798ef393b221500462e397293867ebc802.

--- packages/json/src/resolver.ts
+++ packages/json/src/resolver.ts
@@ -1,8 +1,8 @@
-import type {IfDefined, Resolver} from '@typeschema/core';
+import type {Resolver} from '@typeschema/core';
 import type {SchemaObject} from 'ajv';
 
 export interface AdapterResolver extends Resolver {
-  base: IfDefined<SchemaObject, 'ajv'>;
+  base: SchemaObject;
   input: this['schema'] extends this['base'] ? unknown : never;
   output: this['schema'] extends this['base'] ? unknown : never;
 }
--- packages/zod/src/resolver.ts
+++ packages/zod/src/resolver.ts
@@ -1,8 +1,8 @@
-import type {IfDefined, Resolver} from '@typeschema/core';
+import type {Resolver} from '@typeschema/core';
 import type {input, output, ZodType} from 'zod';
 
 export interface AdapterResolver extends Resolver {
-  base: IfDefined<ZodType, 'zod'>;
+  base: ZodType;
   input: this['schema'] extends this['base'] ? input<this['schema']> : never;
   output: this['schema'] extends this['base'] ? output<this['schema']> : never;
 }
